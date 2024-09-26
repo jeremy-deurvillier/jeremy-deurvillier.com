63c53de87e4aec4918148054927036f6e2942b65
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-09-26 10:30:31 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'htpps://jeremy-deurvillier.com',
+    server: {host: true, port: 3000}
+});
