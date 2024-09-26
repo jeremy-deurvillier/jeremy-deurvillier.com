20aba6bfdddcbecbffc80c8aedf571190bd305c0
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-09-26 11:03:40 +0200

    Add Tailwind CSS package. Hello World !

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'htpps://jeremy-deurvillier.com',
-    server: {host: true, port: 3000}
+  site: 'htpps://jeremy-deurvillier.com',
+  server: {host: true, port: 3000},
+  integrations: [tailwind()]
 });
