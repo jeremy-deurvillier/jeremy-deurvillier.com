10c029a41e96b3435d36f52760162ff543c00352
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-09-27 16:10:28 +0200

    Add Navbar component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'htpps://jeremy-deurvillier.com',
   server: {host: true, port: 3000},
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpinejs()]
 });
