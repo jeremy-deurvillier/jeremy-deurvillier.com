44167141211152f5a0d063b99cc91c0ba1da5e67
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-10-23 00:27:06 +0200

    Update astro. Fix error in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'htpps://jeremy-deurvillier.com',
+  site: 'https://jeremy-deurvillier.com',
   server: {host: true, port: 3000},
   integrations: [tailwind(), alpinejs()]
 });
